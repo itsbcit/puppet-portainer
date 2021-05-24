876ced88a230163213a64e7cf60dd0ace103e288
Jesse Weisner <jesse@weisner.ca>
2021-05-24 16:04:05 -0700

    install templated edge agent service

--- manifests/edge_agent.pp
+++ manifests/edge_agent.pp
@@ -4,5 +4,16 @@
 #
 # @example
 #   include portainer::edge_agent
-class portainer::edge_agent {
+class portainer::edge_agent(
+  String $systemd_unit_path,
+  String $systemd_unit_env_file,
+) {
+  file { 'systemd_unit_file':
+    ensure  => file,
+    path    => "${systemd_unit_path}/portainer-edge-agent.service",
+    owner   => root,
+    group   => root,
+    mode    => '0444',
+    content => template('portainer/portainer-edge-agent.service.erb'),
+  }
 }
