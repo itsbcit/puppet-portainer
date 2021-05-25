b77ecdbe74b50ad9a12fe2382fd6e6c4ecb5db8e
Jesse Weisner <jesse@weisner.ca>
2021-05-24 17:12:04 -0700

    add agent config env file

--- manifests/edge_agent.pp
+++ manifests/edge_agent.pp
@@ -7,6 +7,9 @@
 class portainer::edge_agent(
   String $systemd_unit_path,
   String $systemd_unit_env_file,
+  String $data_path,
+  String $edge_id,
+  String $edge_key,
 ) {
 
   file { 'portainer-edge-agent.service':
@@ -22,6 +25,24 @@ class portainer::edge_agent(
     ],
   }
 
+  file { 'portainer-edge-agent env':
+    ensure  => file,
+    path    => $portainer::edge_agent::systemd_unit_env_file,
+    owner   => root,
+    group   => root,
+    mode    => '0444',
+    content => template('portainer/portainer-edge-agent-env.erb'),
+    notify  => Service['portainer-edge-agent'],
+  }
+
+  file { 'portainer-edge-agent-data':
+    ensure => directory,
+    path   => $portainer::edge_agent::data_path,
+    owner  => root,
+    group  => root,
+    mode   => '0750',
+  }
+
   exec { 'edge-agent-daemon-reload':
     command     => 'systemctl daemon-reload',
     path        => '/bin:/sbin:/usr/bin:/usr/sbin',
@@ -32,6 +53,10 @@ class portainer::edge_agent(
     ensure   => 'running',
     enable   => true,
     provider => systemd,
-    require  => File['portainer-edge-agent.service'],
+    require  => [
+      File['portainer-edge-agent.service'],
+      File['portainer-edge-agent-data'],
+      File['portainer-edge-agent env'],
+    ],
   }
 }
